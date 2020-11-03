7bcc60dff9571009f3933b77022d25243c1af531
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-11-03 22:20:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,8 +3,13 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
+<<<<<<< HEAD
     "s" * 10 + string
     #binding.pry
+=======
+    10 * "s" + string
+    binding.pry
+>>>>>>> 7f8ef1f3b7b11711b33b516ac2932b44d6ce8ec3
   else
     string
   end
